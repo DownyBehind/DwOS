c5363d7f8d883258f946196f2923abe53e5915cc
DaWoon Kim <kim1023123@gmail.com>
2021-02-07 20:29:49 +0900

    first commit

---
+++ main/navilnux.c
@@ -0,0 +1,15 @@
+#include <pxa255.h>
+#include <time.h>
+#include <gpio.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+	while(1){
+		printf("hello world!\n");
+		msleep(1000);
+		printf("this is DwOS!\n");
+		msleep(1000);
+	}
+	return 0;
+}
